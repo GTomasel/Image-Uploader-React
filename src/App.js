b705cf0bf9c254a591d5e57903b3895cc09dbd2d
Gabriel Tomasel <gabitomasel@gmail.com>
2022-09-08 20:32:43 -0300

    Hidden input and file select handler configured

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import logo from './logo.svg';
+import { Component } from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <input type="file"></input>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    selectedFile: null
+  }
+
+  selectedFileHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+
+  fileUploadHandler = () => {
+    const fd = new FormData()
+    fd.append('image', this.state.selectedFile, this.state.selectedFile.name)
+    axios.post('', fd, {
+      onUploadProgress: progressEvent => {
+        console.log(`Upload progress ${Math.round(progressEvent.loaded / progressEvent.total * 100)}%`)
+      }
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input style={{ display: 'none' }} type="file" onChange={this.selectedFileHandler} ref={fileInput => this.fileInput = fileInput} />
+          <button onClick={() => this.fileInput.click()}>Select file</button>
+          <button onClick={this.fileUploadHandler}>Upload</button>
+        </header> 
+      </div>
+    );
+  }
 }
 
 export default App;
