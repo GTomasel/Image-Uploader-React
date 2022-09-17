1c89c4fb65a1c9725b7a0bffcd40054778795f90
Gabriel Tomasel <gabitomasel@gmail.com>
2022-09-17 15:50:56 -0300

    added copy to clipboard function and some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { storage } from './firebase'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Toaster, toast } from 'react-hot-toast'
 import { v4 } from 'uuid'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null)
-  const [uploadedImage, setUploadedImage] = useState(null)
+  const [uploadedImageURL, setUploadedImageURL] = useState(null)
+
+  const inputFile = useRef(null)
 
   const uploadImage = () => {
     if (selectedImage == null) return
@@ -14,7 +19,7 @@ function App() {
     uploadBytes(imageRef, selectedImage).then((snapshot) => {
       console.log('Upload complete')
       getDownloadURL(snapshot.ref).then((url) => {
-        setUploadedImage(url)
+        setUploadedImageURL(url)
       })
 
     })
@@ -24,13 +29,38 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input type="file" onChange={(event) => {
-          setSelectedImage(event.target.files[0])
+
+        {uploadedImageURL &&
+          <>
+            <img src={uploadedImageURL} alt="File visualization" />
+            <CopyToClipboard text={uploadedImageURL}>
+              <button onClick={() => toast.success('Copied to clipboard!')}>Copiar enlace</button>
+            </CopyToClipboard>
+          </>
+        }
+
+        <Toaster toastOptions={{
+          className: 'toastNotification'
         }} />
 
-        <button onClick={uploadImage}>Upload</button>
 
-        <img src={uploadedImage} alt="New file"/>
+        <div className="uploadCard">
+          <p className="title">Upload your image</p>
+          <p className="subTitle">File should be Jpeg, Png,...</p>
+          <div className="fileSelectBox" onClick={() => inputFile.current.click()}>
+            <p>Drag &amp; Drop your image here</p>
+          </div>
+          <p className="subTitle">Or</p>
+
+          <input style={{display: 'none'}} type="file" onChange={(event) => {
+            setSelectedImage(event.target.files[0])
+          }} ref={inputFile} />
+          <button onClick={() => inputFile.current.click()}>Select file</button>
+          <button onClick={uploadImage}>Upload</button>
+
+        </div>
+
+
       </header>
     </div>
   );
